5402264dad633cd49e11fdf50e53d62e8eeee471
Marcio Coelho <marciocoelho@gmail.com>
2021-09-03 18:06:38 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule }             from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { HomeComponent }     from "./home/home.component";
+import { SimpleComponent }   from "./simple/simple.component";
+import { CompoundComponent } from "./compound/compound.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "simple", component: SimpleComponent },
+  { path: "compound", component: CompoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
