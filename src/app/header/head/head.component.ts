46592e035a3b70fbfc03ee16f5c1fec90ce73ccc
Marcio Coelho <marciocoelho@gmail.com>
2021-09-05 14:54:29 -0300

    Updated head.component.ts

--- src/app/header/head/head.component.ts
+++ src/app/header/head/head.component.ts
@@ -2,7 +2,8 @@ import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-head",
-  template: `<h1>{{ title }}</h1>`
+  template: `<h1>{{ title }}</h1>`,
+  styles: ["h1 { font-size: 2.7rem }"]
 })
 export class HeadComponent implements OnInit {
   @Input() title: string | undefined;
